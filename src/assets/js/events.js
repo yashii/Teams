6a43f575a3d3eb822080a4594e2b917e52bc9559
Yashi Srivastava <yashi.srivastava1234@gmail.com>
2021-07-08 23:03:41 +0530

    Update events.js

--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -67,8 +67,8 @@ window.addEventListener( 'load', () => {
             let roomLink = `${ location.origin }?room=${ roomName.trim().replace( ' ', '_' ) }_${ helpers.generateRandomString() }`;
 
             //show message with link to room
-            document.querySelector( '#room-created' ).innerHTML = `Room successfully created. Click <a href='${ roomLink }'>here</a> to enter room. 
-                Share the room link with your partners.`;
+            document.querySelector( '#room-created' ).innerHTML = `<p style="color:white; margin-top:5px;"> Room successfully created. Click <a href='${ roomLink }'>here</a> to enter room. 
+                Share the room link with your partners.</p>`;
 
             //empty the values
             document.querySelector( '#room-name' ).value = '';
